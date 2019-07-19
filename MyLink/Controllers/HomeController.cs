6f3feae22dc1adb5e5af3e0ad315ab8101d43b19
Roger Bobbio <bobbio68@hotmail.com>
2019-07-18 19:59:30 -0500

    Add Charts

--- MyLink/Controllers/HomeController.cs
+++ MyLink/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using MyLink.Models;
+using System.Collections.Generic;
+using System;
 
 namespace MyLink.Controllers
 {
@@ -10,6 +12,18 @@ namespace MyLink.Controllers
 
         public ActionResult Index()
         {
+            var list = db.Links.ToList();
+            List<int> categoriasCount = new List<int>();
+            var categorias = list.Select(c => c.LinkCategory.Name).Distinct();
+            foreach (var item in categorias)
+            {
+                Random rnd = new Random();
+                categoriasCount.Add(list.Count(x => x.LinkCategory.Name == item));
+            }
+            var rep = categoriasCount;
+            ViewBag.CATEGORIAS = categorias;
+            ViewBag.CATEGORIASCOUNT = categoriasCount.ToList();
+
             var user = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
             return View(user);
         }
